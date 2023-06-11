1101814e0fc28677ed3a0ef40c391c30378acfa9
KarasawaTakahiro <karasawa@basicinc.jp>
2023-06-11 22:07:07 +0900

    refactor: use const

--- src/coordinate/grid_point.rs
+++ src/coordinate/grid_point.rs
@@ -1,5 +1,7 @@
 use std::convert::From;
 
+use crate::coordinate::TILE_COLUMNS;
+
 #[derive(Default, Debug, PartialEq, Clone, Copy)]
 pub struct GridPoint {
     pub x: usize,
@@ -11,12 +13,12 @@ impl GridPoint {
         Self { x, y }
     }
 
-    pub fn as_index(&self, width: usize) -> usize {
-        self.y * width + self.x
+    pub fn as_index(&self) -> usize {
+        self.y * TILE_COLUMNS + self.x
     }
 
-    pub fn from_index(index: usize, width: usize) -> GridPoint {
-        Self::new(index % width, index / width)
+    pub fn from_index(index: usize) -> GridPoint {
+        Self::new(index % TILE_COLUMNS, index / TILE_COLUMNS)
     }
 }
 
--- src/field.rs
+++ src/field.rs
@@ -5,7 +5,7 @@ use graphics::Transformed;
 use opengl_graphics::GlGraphics;
 use piston::input::{ButtonArgs, ButtonState};
 
-use crate::coordinate::{GridPoint, Point, Size, TILE_COLUMNS, TILE_ROWS, TILE_LENGTH, TILE_SIZE};
+use crate::coordinate::{GridPoint, Point, Size, TILE_COLUMNS, TILE_LENGTH, TILE_SIZE};
 use crate::item::{Machine, MachineFactory, MaterialVariant};
 use crate::natural_resource::{Coal, IronOre, NaturalResource};
 use crate::types::Direction;
@@ -59,7 +59,7 @@ impl Field {
     }
 
     pub fn add_fixture(&mut self, fixture: Box<dyn Machine>, grid_point: GridPoint) {
-        self.tiles[grid_point.as_index(TILE_COLUMNS)].set_fixture(fixture);
+        self.tiles[grid_point.as_index()].set_fixture(fixture);
     }
 
     pub fn add_natural_resource(
@@ -67,7 +67,7 @@ impl Field {
         natural_resource: Box<dyn NaturalResource>,
         grid_point: GridPoint,
     ) {
-        self.tiles[grid_point.as_index(TILE_COLUMNS)].set_natural_resource(natural_resource);
+        self.tiles[grid_point.as_index()].set_natural_resource(natural_resource);
     }
 
     pub fn render(&self, gl: &mut GlGraphics, context: &Context) {
@@ -88,19 +88,17 @@ impl Field {
 
             if let Some(natural_resource) = tile.natural_resource() {
                 let mut context: Context = *context;
-                context.transform = context.transform.trans(
-                    tile.x as f64 * TILE_SIZE,
-                    tile.y as f64 * TILE_SIZE,
-                );
+                context.transform = context
+                    .transform
+                    .trans(tile.x as f64 * TILE_SIZE, tile.y as f64 * TILE_SIZE);
                 natural_resource.render(gl, &context, &tile_size);
             }
 
             if let Some(fixture) = tile.fixture() {
                 let mut context: Context = *context;
-                context.transform = context.transform.trans(
-                    tile.x as f64 * TILE_SIZE,
-                    tile.y as f64 * TILE_SIZE,
-                );
+                context.transform = context
+                    .transform
+                    .trans(tile.x as f64 * TILE_SIZE, tile.y as f64 * TILE_SIZE);
                 fixture.render(gl, &context);
                 context.reset();
             }
@@ -177,14 +175,14 @@ impl Field {
 
             match args.button {
                 piston::Button::Mouse(piston::MouseButton::Left) => {
-                    if let Some(fixture) = &mut self.tiles[point.as_index(TILE_COLUMNS)].fixture_mut() {
+                    if let Some(fixture) = &mut self.tiles[point.as_index()].fixture_mut() {
                         fixture.on_click();
                     } else if let Some(item) = holding_item {
                         self.add_fixture(item, point);
                     }
                 }
                 piston::Button::Keyboard(piston::Key::R) => {
-                    if let Some(fixture) = &mut self.tiles[point.as_index(TILE_COLUMNS)].fixture_mut() {
+                    if let Some(fixture) = &mut self.tiles[point.as_index()].fixture_mut() {
                         fixture.rotate();
                     }
                 }
@@ -359,7 +357,7 @@ impl Iterator for TileIterator {
             TileIteratorState::None => None,
             TileIteratorState::Range(range) => {
                 for point in range {
-                    let index = point.as_index(TILE_COLUMNS);
+                    let index = point.as_index();
                     return Some(index);
                 }
                 None
@@ -372,7 +370,7 @@ impl Iterator for TileIterator {
 mod test_tile_iterator {
     use crate::{
         coordinate::GridPoint,
-        field::{TILE_LENGTH, TILE_COLUMNS},
+        field::TILE_LENGTH,
         item::{MachineFactory, MaterialVariant},
         tile::Tile,
     };
@@ -384,25 +382,25 @@ mod test_tile_iterator {
         let mut tiles = Vec::new();
         for i in 0..(TILE_LENGTH) {
             let mut tile: Tile = Default::default();
-            let point = GridPoint::from_index(i, TILE_COLUMNS);
+            let point = GridPoint::from_index(i);
             tile.x = point.x;
             tile.y = point.y;
             tiles.push(tile);
         }
-        let index = GridPoint::new(2, 2).as_index(TILE_COLUMNS);
+        let index = GridPoint::new(2, 2).as_index();
         tiles[index].set_fixture(MachineFactory::build(MaterialVariant::Conveyer).unwrap());
 
         let mut iter = TileIterator::new(index, &tiles);
 
-        assert_eq!(iter.next(), Some(GridPoint::new(1, 1).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(2, 1).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(3, 1).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(1, 2).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(2, 2).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(3, 2).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(1, 3).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(2, 3).as_index(TILE_COLUMNS)));
-        assert_eq!(iter.next(), Some(GridPoint::new(3, 3).as_index(TILE_COLUMNS)));
+        assert_eq!(iter.next(), Some(GridPoint::new(1, 1).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(2, 1).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(3, 1).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(1, 2).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(2, 2).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(3, 2).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(1, 3).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(2, 3).as_index()));
+        assert_eq!(iter.next(), Some(GridPoint::new(3, 3).as_index()));
         assert_eq!(iter.next(), None);
     }
 }
