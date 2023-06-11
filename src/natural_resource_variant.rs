040bc79ed53000cc8145978bb16efd68da630e14
KarasawaTakahiro <karasawa@basicinc.jp>
2023-06-11 17:45:01 +0900

    feat: NaturalResourceVariant become ItemVariant

--- src/natural_resource_variant.rs
+++ src/natural_resource_variant.rs
@@ -1,4 +1,14 @@
+use crate::item::MaterialVariant;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum NaturalResourceVariant {
     IronOre,
+}
+
+impl From<NaturalResourceVariant> for MaterialVariant {
+    fn from(variant: NaturalResourceVariant) -> Self {
+        match variant {
+            NaturalResourceVariant::IronOre => MaterialVariant::IronOre,
+        }
+    }
 }
